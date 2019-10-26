fae850f8d445d8d434caddb7e76a6b669c68a686
George Pasley <gpasley@imazuma.com>
2019-10-26 00:46:08 -0400

    added locations to map and layers with new hotspots table

--- Create_table.sql
+++ Create_table.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS restaurants;
 DROP TABLE IF EXISTS attractions;
 DROP TABLE IF EXISTS top_rated_restaurant;
 DROP TABLE IF EXISTS tourist_attraction;
+DROP TABLE IF EXISTS hotspots;
 
 -- Create TABLE  LYNX
 CREATE table lynx (
@@ -28,4 +29,13 @@ CREATE TABLE attractions (
   Name VARCHAR(100) NOT NULL,
   Latitude VARCHAR(100) NOT NULL,
   Longitude VARCHAR(100) NOT NULL
+);
+
+-- Create table hotspots
+CREATE TABLE hotspots (
+  id SERIAL PRIMARY KEY,
+  Name VARCHAR(100) NOT NULL,
+  Latitude VARCHAR(100) NOT NULL,
+  Longitude VARCHAR(100) NOT NULL,
+  Category VARCHAR (255) NOT NULL
 );
